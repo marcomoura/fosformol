eb61e8a4f54afc71e2d80b6e74ece358b87c362b
Bruno Vicenzo, Marco Moura, and Roberto Oliveira <dev+bruno+marco+roberto@gonow.com.br>
2012-07-06 15:05:41 -0300

    action para finalizar um sprint e iniciar um novo

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -6,6 +6,15 @@ class SprintsController < ApplicationController
     @sprints = @project.sprints
   end
 
+  def destroy
+    @sprint = Sprint.find(params[:id])
+    if @sprint.finalize
+      redirect_to admin_project_path(@project), :notice => "Sprint successfully completed!"
+    else
+      redirect_to admin_project_path(@project), :alert => "Sprint successfully completed!"
+    end
+  end
+
   protected
 
   def load_project
--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -3,6 +3,14 @@ class Sprint < ActiveRecord::Base
   belongs_to :project
 
   validates :project_id, :iteration, :active, :presence => true
+  validates :iteration, :uniqueness => {:scope => :project_id}
+
+  def finalize
+    self.update_attributes(:active => false)
+    new_sprint = Sprint.create(:iteration => self.iteration.next, :project_id => self.project_id)
+
+    new_sprint.valid?
+  end
 
   def to_s
     "sprint #{self.iteration}"
