c2d510b6100471696a17861b464ce03149d33643
Bruno Vicenzo, Marco Moura, and Roberto Oliveira <dev+bruno+marco+roberto@gonow.com.br>
2012-07-06 15:38:54 -0300

    inativar sprint atual e criar proximo funcionando

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,11 +2,13 @@ class Sprint < ActiveRecord::Base
   has_many   :feedbacks
   belongs_to :project
 
-  validates :project_id, :iteration, :active, :presence => true
+  validates :project_id, :iteration, :presence => true
   validates :iteration, :uniqueness => {:scope => :project_id}
 
+  default_scope order('iteration DESC')
+
   def finalize
-    self.update_attributes(:active => false)
+    return false unless self.update_attributes(:active => false)
     new_sprint = Sprint.create(:iteration => self.iteration.next, :project_id => self.project_id)
 
     new_sprint.valid?
