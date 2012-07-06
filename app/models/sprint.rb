899320d2c5a5d82773ebf83c3d1502dbda338912
Bruno Vicenzo, Marco Moura, and Roberto Oliveira <dev+bruno+marco+roberto@gonow.com.br>
2012-07-06 14:47:45 -0300

    adicionando validações nos models Project e Sprint

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,6 +2,8 @@ class Sprint < ActiveRecord::Base
   has_many   :feedbacks
   belongs_to :project
 
+  validates :project_id, :iteration, :active, :presence => true
+
   def to_s
     "sprint #{self.iteration}"
   end
