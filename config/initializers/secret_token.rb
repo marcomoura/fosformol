bb4033c1aacfa2f8b1d5ddb82dd11e4cb4a35ed4
Bruno Vicenzo <bruno.nascimento@gonow.com.br>
2012-07-06 11:12:39 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fosformol::Application.config.secret_token = 'd4b78e654de22b1446f5bc627ecb1220da0dfc8dceda977cdaa34e17260bdf0b52e5f438e03ec5cb7bcb7c002405d49a1024cb97b551e98fa2a57cd6087d738f'
