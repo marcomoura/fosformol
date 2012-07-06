f5e5311691f02f11d7008051912489699ba1940a
Marco Moura <email@marcomoura.com>
2012-07-06 14:06:29 -0300

    adicionado active admin

---
+++ app/admin/projects.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Project do
+
+  #index :as => :grid do |project|
+  #link_to(project.title, admin_project_path(project))
+  #end
+
+  #index :as => :block do |project|
+    #div :for => project do
+      #h2 auto_link(project.title)
+    #end
+  #end
+
+
+  index do
+    column "Project" do |project|
+      link_to project.title, admin_project_path(project)
+    end
+    column "Actions" do |project|
+      link_to 'Edit', edit_admin_project_path(project)
+      link_to 'Destroy', project, :confirm => 'Are you sure?', :method => :delete
+    end
+  end
+end
